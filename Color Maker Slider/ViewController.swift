4650490d4e2fc202a85871591d883bb225ce1a33
omrobbie <putu.roby@yahoo.com>
2018-04-06 10:43:34 +0800

    add method for mix the color

--- Color Maker Slider/ViewController.swift
+++ Color Maker Slider/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet var colorView : UIView!
+    
+    @IBOutlet var redControl : UISlider!
+    @IBOutlet var greenControl : UISlider!
+    @IBOutlet var blueControl : UISlider!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +24,15 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func changeColorComponent(_ sender: AnyObject) {
+        
+        let r: CGFloat = CGFloat(self.redControl.value)
+        let g: CGFloat = CGFloat(self.greenControl.value)
+        let b: CGFloat = CGFloat(self.blueControl.value)
+        
+        colorView.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1)
     }
 
 
